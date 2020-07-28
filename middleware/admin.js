6fb4b89634f4f6804747dbde6153a1952229596b
jjginga <jjginga@gmail.com>
2020-07-28 18:28:58 +0100

    added disabling authentication for development purposes

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,4 +1,8 @@
+const config = require('config');
+
 module.exports = function (req, res, next) {
-  if (!req.user.isAdmin) return res.status(403).send("Acess denied,");
+  if (!config.get('requiresAuth')) return next();
+
+  if (!req.user.isAdmin) return res.status(403).send('Access denied,');
   next();
 };
