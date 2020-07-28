c9d5c26778965fd1201921212d20c9d7ffbfb860
jjginga <jjginga@gmail.com>
2020-07-28 18:34:40 +0100

    added disabling authentication for development purposes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,21 @@
-const config = require("config");
-const jwt = require("jsonwebtoken");
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 module.exports = function auth(req, res, next) {
-  const token = req.header("x-auth-token");
+  if (!config.get('requiresAuth')) return next();
 
-  if (!token) return res.status(401).send("No token provided. Access denied");
+  const token = req.header('x-auth-token');
+
+  if (!token) return res.status(401).send('No token provided. Access denied');
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
 
     //we get the { _id: this._id } object
 
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(400).send("Invalid token.");
+    res.status(400).send('Invalid token.');
   }
 };
